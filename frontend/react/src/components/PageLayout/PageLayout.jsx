a09968d1f8409b16a23bae9e9a70fd26f769369e
Sammy <96604660+itsmasso@users.noreply.github.com>
2025-06-18 00:53:58 -0700

    Update PageLayout.jsx

--- frontend/react/src/components/PageLayout/PageLayout.jsx
+++ frontend/react/src/components/PageLayout/PageLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../Sidebar/sidebar";
+import Sidebar from "../Sidebar/Sidebar";
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router";
 
